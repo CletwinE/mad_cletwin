d18f04e2ae10ef2a5b21f57b117da79e1df28320
Cletwin <53634739+CletwinE@users.noreply.github.com>
2019-08-09 22:12:32 +0530

    Okk

---
+++ app/src/main/java/com/example/admin/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), explore.class));
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/admin/explore.java
@@ -0,0 +1,20 @@
+package com.example.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class explore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explore);
+    }
+
+
+
+
+}
